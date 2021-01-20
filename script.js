7e38318cdcdf81e97fb13643b2be3a5c138b07ac
Frank Merchlewitz <jlimhb@gmail.com>
2021-01-20 17:51:45 -0600

    Figured out Luxon to the point that I could write the local current date to the page

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-var stringLuxon = JSON.stringify(luxon);
-console.log(stringLuxon);
-console.log(luxon.DateTime);
-
 var DateTime = luxon.DateTime;
 var dt = DateTime.fromISO(new Date().toISOString());
 console.log(dt.toFormat('yyyy/MM/dd'));
 var dt2 = DateTime.fromJSDate(new Date());
 console.log(dt2.toFormat('yyyy/MM/dd'));
+
+var currentDate = dt.toLocaleString(DateTime.DATETIME_MED);
+console.log(currentDate);
+
+function displayCurrentDate() {
+    $("#currentDay").text(currentDate);
+  }
+
+  displayCurrentDate();
